bbd6300f0f73964ebc7aa2f11c676c563f13cc1d
LeoCustodio <leoccustodio@gmail.com>
2026-02-20 17:43:02 -0300

    feature: Improving GitHub Code performance.

--- src/jira_github_agent/tools/github_tools.py
+++ src/jira_github_agent/tools/github_tools.py
@@ -18,7 +18,7 @@ def build_github_tools(gh: GitHubClient):
     def github_create_pr(owner: str, repo: str, head: str, base: str, title: str, body: str="") -> Dict[str, Any]:
         """Create Pull request in the github repository provided with the code generated"""
         url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
-        resp = gh.post(url, {"title": title, "head": head, "base": base, "body": body})
+        resp = gh.post(url, {"title": title, "body": body, "head": head, "base": base, })
         return {"url": resp.get("html_url"), "number": resp.get("number")}
 
     return [github_create_pr]
